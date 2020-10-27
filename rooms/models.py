b72b33f7534a50b96d44f37e3e4839e28cb4e841
Gonzalo Romero <gonclapton@hotmail.com>
2020-10-27 16:28:58 -0600

    ADD rooms model

---
+++ rooms/models.py
@@ -0,0 +1,23 @@
+# Django
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.utils import timezone
+
+# Define choices for status field
+STATUS = (
+    ('OPEN', 'OPEN'),
+    ('RESERVED', 'RESERVED'),
+)
+
+
+class Room(models.Model):
+    room_name = models.CharField(max_length=200)
+    date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    status = models.CharField(max_length=8, choices=STATUS)
+    minutes_estimated = models.IntegerField(validators=[MaxValueValidator(180), MinValueValidator(1)])
+    current_time = models.TimeField(default=timezone.now())
+
+    def __str__(self):
+        return self.room_name
